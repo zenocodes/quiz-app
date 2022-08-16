fb747388a170c543aaecbca58cac1a6762ee0a5c
zenocodes <azronebrian@gmail.com>
2022-08-16 13:37:56 +0300

    added validations: password mismatch

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
+import { render } from 'ejs'
 import express from 'express'
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+
+
+const users = [
+    { 
+        name: 'Jina Lake', 
+        email: 'jl@quiz-app.com', 
+        password: 'test1234'
+    },
+    { 
+        name: 'Yule Mwingine', 
+        email: 'ym@quiz-app.com', 
+        password: 'test1234'
+    }
+]
 
 // landing page
 app.get('/', (req, res) => {
@@ -17,7 +33,30 @@ app.get('/login', (req, res) => {
 
 // display signup page
 app.get('/signup', (req, res) => {
-    res.render('signup')
+    const user = {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    }
+    res.render('signup', {error: false, user: user})
+})
+
+// process signup form
+app.post('/signup', (req, res) => {
+    const user = {
+        name: req.body.fullname,
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword
+    }
+
+    if (user.password === user.confirmPassword) {
+        
+    } else {
+        let message = 'Password/confirm password mismatch'
+        res.render('signup', {error: true, message: message, user: user})
+    }
 })
 
 
