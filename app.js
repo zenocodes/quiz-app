1024b757ece5224e540a471de24eb726d82e78e6
zenocodes <azronebrian@gmail.com>
2022-08-16 11:00:40 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+// landing page
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+    console.log(`app is running on PORT ${PORT}`)
+})
