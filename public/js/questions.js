db5b66df6caedca4ce5b7f4f082f0f916a31a415
zenocodes <azronebrian@gmail.com>
2022-08-26 12:24:27 +0300

    added hidden input & submit button

--- public/js/questions.js
+++ public/js/questions.js
@@ -7,12 +7,12 @@ questions.forEach(question => {
     div.className = 'form-input'
     let p = document.createElement('p')
     p.className = 'question'
-    p.textContent = question.question
+    p.textContent = `${question.id}. ${question.question}`
     let choices = document.createElement('div')
     question.choices.forEach(choice => {
         let div = document.createElement('div')
         div.className = 'choice'
-        div.innerHTML = `<input type="radio" name="q${question.id}"> ${choice}`
+        div.innerHTML = `<input type="radio" name="q${question.id}" value="${choice}"> ${choice}`
         div.addEventListener('click', () => {
             // div.innerHTML = `<input type="radio" name="q${question.id}" checked> ${choice}`
             document.getElementsByName(`q${question.id}`).forEach(radioBtn => radioBtn.removeAttribute('checked'))
@@ -24,3 +24,12 @@ questions.forEach(question => {
     div.appendChild(choices)
     form.appendChild(div)
 })
+
+let submitBtn = document.createElement('input')
+submitBtn.setAttribute('type', 'submit')
+submitBtn.setAttribute('value', 'Submit Quiz')
+
+let markingScheme = document.querySelector('#markingScheme')
+markingScheme.value = questions.map(question => question.answer)
+
+form.appendChild(submitBtn)
